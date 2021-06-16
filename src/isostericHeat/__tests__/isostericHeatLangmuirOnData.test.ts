3ffb432099b291f96655841ced5457c0bf321aee
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-06-16 14:45:14 +0200

    chore: generalized isosteric heat to accept an indefinite number of datasets

--- src/isostericHeat/__tests__/isostericHeatLangmuirOnData.test.ts
+++ src/isostericHeat/__tests__/isostericHeatLangmuirOnData.test.ts
@@ -1,11 +1,11 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
-import langmuirFunction from '../../modelFunctions/langmuirSingleFunction';
 
+import langmuirFunction from '../../modelFunctions/langmuirSingleFunction';
 import isostericHeatLangmuirOnData from '../isostericHeatLangmuirOnData';
 
 describe('test Langmuir fit', () => {
-  it.only('first dataSet', () => {
+  it('first dataSet', () => {
     let rawData = readFileSync(join(__dirname, './data/data.json'));
     let data = JSON.parse(rawData.toString());
 
@@ -14,11 +14,68 @@ describe('test Langmuir fit', () => {
     data.p2 = data.p2.map((p: number) => p * 1000);
     data.p3 = data.p3.map((p: number) => p * 1000);
 
-    let deltaH = isostericHeatLangmuirOnData(
+    let deltaH = isostericHeatLangmuirOnData([
       { T: T1, x: data.p1, y: data.n },
       { T: T2, x: data.p2, y: data.n },
       { T: T3, x: data.p3, y: data.n },
+    ]);
+    console.log(deltaH);
+    writeFileSync(
+      join(__dirname, '../../../examples/data1.json'),
+      JSON.stringify({ x: data.p1, y: data.n }),
     );
+    writeFileSync(
+      join(__dirname, '../../../examples/data1SIM.json'),
+      JSON.stringify({
+        x: data.p1,
+        y: data.p1.map((x: number) =>
+          langmuirFunction([deltaH.x[0], deltaH.x[3]])(x),
+        ),
+      }),
+    );
+
+    writeFileSync(
+      join(__dirname, '../../../examples/data2.json'),
+      JSON.stringify({ x: data.p2, y: data.n }),
+    );
+    writeFileSync(
+      join(__dirname, '../../../examples/data2SIM.json'),
+      JSON.stringify({
+        x: data.p2,
+        y: data.p2.map((x: number) =>
+          langmuirFunction([deltaH.x[1], deltaH.x[3]])(x),
+        ),
+      }),
+    );
+    writeFileSync(
+      join(__dirname, '../../../examples/data3.json'),
+      JSON.stringify({ x: data.p3, y: data.n }),
+    );
+    writeFileSync(
+      join(__dirname, '../../../examples/data3SIM.json'),
+      JSON.stringify({
+        x: data.p3,
+        y: data.p3.map((x: number) =>
+          langmuirFunction([deltaH.x[2], deltaH.x[3]])(x),
+        ),
+      }),
+    );
+    //for (let i = 0; i < p1.length; i++) {expect(deltaH[i]).toBeCloseTo(data.Hads[i]);}
+  });
+  it.only('simulated dataset', () => {
+    let rawData = readFileSync(join(__dirname, './data/data.json'));
+    let data = JSON.parse(rawData.toString());
+
+    let [T1, T2, T3] = [273, 283, 293];
+    data.p1 = data.p1.map((p: number) => p * 1000);
+    data.p2 = data.p2.map((p: number) => p * 1000);
+    data.p3 = data.p3.map((p: number) => p * 1000);
+
+    let deltaH = isostericHeatLangmuirOnData([
+      { T: T1, x: data.p1, y: data.n },
+      { T: T2, x: data.p2, y: data.n },
+      { T: T3, x: data.p3, y: data.n },
+    ]);
     console.log(deltaH);
     writeFileSync(
       join(__dirname, '../../../examples/data1.json'),
--- src/isostericHeat/loss/langmuirSingleLoss.ts
+++ src/isostericHeat/loss/langmuirSingleLoss.ts
@@ -1,7 +1,7 @@
 import langmuirFunction from '../../modelFunctions/langmuirSingleFunction';
-export default function langmuirSingleLoss(data: any) {
-  let yHat, y;
 
+export default function langmuirSingleLoss(data: any[]) {
+  let yHat, y;
   return function totalLoss(params: number[]) {
     let cumulatedLoss = 0;
     for (let i = 0; i < data.length; i++) {
