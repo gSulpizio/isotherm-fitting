9ad5223114dee751dabe6b9399b66fbfb6f49fdb
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-06-18 23:08:33 +0200

    Fix: the bug that kept me awake at night has been found, I AM happiness
    https://www.youtube.com/watch?v=ZbZSe6N_BXs

--- src/isostericHeat/loss/langmuirSingleLoss.ts
+++ src/isostericHeat/loss/langmuirSingleLoss.ts
@@ -1,5 +1,5 @@
-import langmuirSingleFunction from '../../modelFunctions/langmuirSingleFunction';
 import logParameters from '../../../dev tools/logParameters';
+import langmuirSingleFunction from '../../modelFunctions/langmuirSingleFunction';
 
 /**
  * Curried function that returns a cumulated loss function
@@ -14,14 +14,15 @@ export default function langmuirSingleLoss(data: any[]) {
     for (let i = 0; i < data.length; i++) {
       for (let p = 0; p < data[i].x.length; p++) {
         yHat = langmuirSingleFunction([params[i], params[params.length - 1]])(
-          p,
+          data[i].x[p],
         );
         y = data[i].y[p];
         cumulatedLoss += (yHat - y) ** 2;
       }
     }
-    //LOGGING PARAMETERS:
 
+    //if (params.some((item) => item <= 0)) {cumulatedLoss += 1000000;}
+    //LOGGING PARAMETERS:
     logParameters(params, cumulatedLoss);
     return cumulatedLoss;
   };
