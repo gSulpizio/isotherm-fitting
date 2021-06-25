e74e36f39e690960e4ba81301fea4125f678687b
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-06-26 01:18:57 +0200

    chore: update isostericHeatFromData docstrings

--- src/isostericHeat/isostericHeatFromData.ts
+++ src/isostericHeat/isostericHeatFromData.ts
@@ -7,10 +7,8 @@ import getConstants from '../variousTools/getConstants';
 
 /**
  * evaluates the isosteric heat of adsorption using the langmuir equation on the isotherm's real data. Takes pressures from 3 isotherms as an input
- * @param {Array<Number>} p1 pressures of isotherm at T1, in kPa
- * @param {Array<Number>} p2 pressures of isotherm at T2, in kPa
- * @param {Array<Number>} p3 pressures of isotherm at T3, in kPa
- * @param {Array<Number>} [T1,T2,T3] temperatures T1,T2,T3
+ * @param {Array} data array of all the data set objects
+ * @param {string} functionName string containing the name of the desired function
  * @returns {Number} isosteric heat of adsorption
  */
 export default function isostericHeatFromData(
