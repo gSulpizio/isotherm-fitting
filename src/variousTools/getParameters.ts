7ea582d242f7f84b10f2909c7956cfd6fad7b146
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-06-26 01:31:35 +0200

    Chore: update docstring getParameters

--- src/variousTools/getParameters.ts
+++ src/variousTools/getParameters.ts
@@ -1,11 +1,16 @@
 import getN from '../isostericHeat/loss/getN';
 
 /**
- * gives parameters array for a specific data set from an array containing all the parameters for all the data sets
+ * gives parameters array for a specific data set from an array containing all the parameters for all the data sets.
+ * This is needed as there are not the same number of parameters to optimize for all the different functions:
+ * Example: for langmuir single site, there are 2 parameters for each data set where as for triple site there are 6 parameters for each data set.
+ *
+ * This function gives therefore an array with the right parameters for each data set, as they are all in one big array for the optimization
+ *
  * @param {string} functionName name of the function that will be used
  * @param {number} i indice of the dataset in a data array
- * @param {Array} parameterList array containing list of parameters
- * @returns {Array} array of the parameters to use in a function,
+ * @param {Array} parameterList array containing  all the parameters for the whole data
+ * @returns {Array} array of the parameters to use in a function
  */
 export default function getParameters(
   functionName: string,
