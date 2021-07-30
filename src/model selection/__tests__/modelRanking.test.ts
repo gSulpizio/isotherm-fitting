cf85a976dbf5fd528755b0a61570886716f2de6c
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-07-30 13:11:53 +0200

    feat: add modelRanking

---
+++ src/model selection/__tests__/modelRanking.test.ts
@@ -0,0 +1,5 @@
+import modelRanking from "../modelRanking"
+
+test('testing mondelRanking, please work',()=>{
+    modelRanking()
+})
---
+++ src/model selection/modelRanking.ts
@@ -0,0 +1,5 @@
+import * as things from '../langmuir'
+
+export default function modelRanking(){
+    console.log(things)
+}
