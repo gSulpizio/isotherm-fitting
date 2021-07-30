1206b012e056491678a442f5920f1eaae925ed2d
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-07-30 14:10:52 +0200

    chore: doc

--- src/isostericHeat/loss/__tests__/getlnP.test.ts
+++ src/isostericHeat/loss/__tests__/getlnP.test.ts
@@ -1,5 +1,5 @@
 import makeNoisyData from '../../../variousTools/makeNoisyData';
-import getnlnP from '../../getlnP';
+import getlnP from '../../getlnP';
 
 describe('test getlnP', () => {
   it('on simulated data', () => {
@@ -9,7 +9,7 @@ describe('test getlnP', () => {
     let data: looseObject = makeNoisyData([3, 5], 150);
     data.T = 187;
 
-    let loadings = getnlnP([data], 'langmuirSingle', [3, 5]);
+    let loadings = getlnP([data], 'langmuirSingle', [3, 5]);
     expect(data.lnP[32]).toBeCloseTo(Math.log(data.x[10]), 1);
   });
   it('On multiple dataSets', () => {
@@ -20,7 +20,7 @@ describe('test getlnP', () => {
     data1.T = 187;
     let data2: looseObject = makeNoisyData([2, 5], 150);
     data2.T = 197;
-    let loadings = getnlnP([data1, data2], 'langmuirSingle', [3, 2, 5]);
+    let loadings = getlnP([data1, data2], 'langmuirSingle', [3, 2, 5]);
 
     expect(data1.lnP[133]).toBeCloseTo(Math.log(data1.x[75]), 1);
     expect(data1.lnP[12]).toBeCloseTo(-3.88617455269805, 1);
