084e3bff9ad7c7873457d214ba48166dd92e0bb0
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-08-10 18:06:17 +0200

    chore: add test

---
+++ src/model selection/__tests__/getMSEstd.test.ts
@@ -0,0 +1,16 @@
+import getMSEstd from '../getMSEstd';
+import looseData from '../looseData';
+
+test('test getMSEstd', () => {
+  let data: looseData = [
+    { name: 'Ferrari 250 GT Berlinetta SWB', MSE: 0 },
+    { name: 'fiat 509', MSE: 10 },
+    { name: 'Citroen Trèfle', MSE: 35 },
+  ];
+  let result = getMSEstd(data);
+  expect(result).toStrictEqual(
+    Math.sqrt(
+      ((0 - 45 / 3) ** 2 + (10 - 45 / 3) ** 2 + (35 - 45 / 3) ** 2) / 3,
+    ),
+  );
+});
