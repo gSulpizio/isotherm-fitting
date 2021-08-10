337799a4b8ad3095f30ff9c91b4302d7dceab0ae
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-08-10 10:18:15 +0200

    chore: documentation

--- src/model selection/AIC.ts
+++ src/model selection/AIC.ts
@@ -1,17 +1,16 @@
 import isotherm from '../isotherm';
 
-
 /**
  * Computes the Akaike information criterion: https://en.wikipedia.org/wiki/Akaike_information_criterion
  * @param {isotherm} data: isotherm with at least x (pressure ) and y (loading) arrays
- * @param {string} functionName: string containing the name of the model to be evaluated
+ * @param {number} MSE: Mean Squared Error between data and model
  * @param {Array<number>} fittedParams: array of the parameters that were fitted
  * @returns {number} AIC
  */
 
 export default function AIC(
   data: isotherm,
-  MSE:number,
+  MSE: number,
   fittedParams: number[],
 ) {
   return data.x.length * Math.log(MSE) + 2 * fittedParams.length;
